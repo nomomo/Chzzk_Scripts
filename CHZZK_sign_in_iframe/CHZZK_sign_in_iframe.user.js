5b6d6121eb167634eb21cabe563ecb7e15bf83e7
Nomotg <nomotg@gmail.com>
2024-01-06 14:00:57 +0900

    CHZZK_sign_in_iframe v0.0.1

---
+++ CHZZK_sign_in_iframe/CHZZK_sign_in_iframe.user.js
@@ -0,0 +1,103 @@
+// ==UserScript==
+// @name         CHZZK_sign_in_iframe
+// @namespace    CHZZK_sign_in_iframe
+// @version      0.0.1
+// @description  iframe 으로 삽입된 CHZZK 페이지에서 로그인이 유지되도록 합니다.
+// @author       Nomo
+// @match        https://chzzk.naver.com/*
+// @supportURL   https://github.com/nomomo/Chzzk_Scripts/issues
+// @homepage     https://github.com/nomomo/Chzzk_Scripts/
+// @downloadURL  https://github.com/nomomo/Chzzk_Scripts/raw/main/CHZZK_sign_in_iframe/CHZZK_sign_in_iframe.user.js
+// @updateURL    https://github.com/nomomo/Chzzk_Scripts/raw/main/CHZZK_sign_in_iframe/CHZZK_sign_in_iframe.user.js
+// @grant        GM_cookie
+// @grant        GM.cookie
+// @run-at       document-end
+// ==/UserScript==
+
+(async function () {
+    'use strict';
+
+    var DEBUG = false;
+    const targetCookies = ["NID_AUT", "NID_SES", "NID_JKL"];
+
+    if (!GM.cookie) {
+        console.log("GM.cookie is not supported.");
+        return;
+    }
+
+    if (DEBUG) {
+        GM.cookie.list({}).then(function (cookies, error) {
+            console.log("get all coockies", cookies, error);
+        });
+    }
+
+    function setCookieSameSiteNone(targetCookieName, options) {
+        GM.cookie.list({
+            name: targetCookieName
+        }).then(function (cookies, error) {
+            if (!error) {
+                for (let i = 0; i < cookies.length; i++) {
+                    if(cookies[i].sameSite == "no_restriction" && cookies[i].secure == true){
+                        continue;
+                    }
+
+                    if (DEBUG) console.log("Try to delete old cookie", cookies[0]);
+                    GM_cookie.delete(cookies[i], function () {
+                        
+                        cookies[i].sameSite = "no_restriction";
+                        cookies[i].secure = true;
+                        
+                        if (DEBUG) console.log("Try to set new cookie", cookies[0]);
+                        GM.cookie.set(cookies[i])
+                            .then(function () {
+                                if (DEBUG) console.log('set cookie done');
+                            }, function (error) {
+                                if (DEBUG) console.log('set cookie error', error);
+                            });
+
+                    })
+                }
+            }
+            else{
+                if (DEBUG) console.log('error from GM.cookie.list of setCookieSameSiteNone');
+            }
+        });
+    }
+
+    for (const ck of targetCookies) {
+        setCookieSameSiteNone(ck);
+    }
+
+    let isTopWindow = window.self === window.top;
+
+    // 쿠키 변경 시 갱신
+    if (cookieStore) {
+        cookieStore.addEventListener("change", function (event) {
+            if (DEBUG) console.log("cookie change event", event.changed);
+
+            if (event.changed.length > 0 && event.changed[0].sameSite != "none" && targetCookies.includes(event.changed[0].name)) {
+                setCookieSameSiteNone(event.changed[0].name);
+            }
+
+        });
+    } else {
+        console.log("cookieStore is not supported.");
+        if(isTopWindow){
+            setInterval(function () {
+                for (const ck of targetCookies) {
+                    setCookieSameSiteNone(ck);
+                }
+            }, 250);
+        }
+    }
+
+    // 전혀 상관없는 다른 창에서 쿠키 변경된 경우 때문에 문제 발생하는 경우를 방지
+    if(!isTopWindow){
+        setInterval(function () {
+            for (const ck of targetCookies) {
+                setCookieSameSiteNone(ck);
+            }
+        }, 250);
+    }
+
+})();
---
+++ CHZZK_sign_in_iframe/README.md
@@ -0,0 +1,34 @@
+# CHZZK_sign_in_iframe
+
+- iframe 으로 삽입된 CHZZK 페이지에서 로그인이 유지되도록 합니다.
+- 로그인이 유지되므로 연령 인증 스트림 시청, 채팅이 가능합니다.
+- 로그인은 chzzk.naver.com 에서 직접해야 합니다.
+- **본 스크립트는 Chrome 계열 브라우저의 경우 Tampermonkey Beta 버전에서만 동작합니다. (스크립트 매니저가 GM_cookie 기능을 지원해야 함)**
+
+## Install
+
+설치 방법을 설명합니다.
+
+### STEP 1. ScriptManager
+
+Chrome 계열 브라우저의 경우 Tampermonkey Beta 버전에서만 작동합니다. 아래 링크에서 Tampermonkey Beta 를 설치하세요.
+
+- Chrome - [Tampermonkey Beta](https://chromewebstore.google.com/detail/tampermonkey-beta/gcalenpjmijncebpfijmoaglllgpjagf)
+
+### STEP 2. UserScript
+
+- 유저스크립트 관리 확장기능 설치 후 아래의 링크를 클릭하세요. 이후 뜨는 창에서 "설치" 버튼을 눌러 스크립트를 설치합니다.
+  - [https://github.com/nomomo/Chzzk_Scripts/raw/main/CHZZK_sign_in_iframe/CHZZK_sign_in_iframe.user.js](https://github.com/nomomo/Chzzk_Scripts/raw/main/CHZZK_sign_in_iframe/CHZZK_sign_in_iframe.user.js)
+
+이것으로 설치는 끝입니다. 즐겁게 사용하세요~
+
+> 주의: 본 스크립트를 설치 및 사용하며 브라우저 과부하로 인한 응답 없음/뻗음으로 인한 데이터 손실, 보안 이슈, 기타 발생하는 다른 문제에 대하여 개발자는 책임지지 않음(보고된 문제는 없음)  
+> 본 스크립트는 Tampermonkey 외의 스크립트 매니저에서는 정상 동작하지 않을 수 있습니다.
+
+### 0.0.1 - Jac. 06, 2024
+
+- 최초 커밋
+
+## Happy??
+
+<a href="https://www.buymeacoffee.com/nomomo" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/default-yellow.png" alt="Buy Me A Coffee" height="60"></a>
