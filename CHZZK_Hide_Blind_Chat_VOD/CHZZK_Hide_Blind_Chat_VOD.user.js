f0946bcf16cc55a70f3f07cef81954fe4d7c269d
Nomo <nomotg@gmail.com>
2024-09-30 00:49:33 +0900

    CHZZK_Restore_Blind_Chat: 0.0.2 - Sep. 29, 2024 - CSS를 이용하여 구현하도록 변경

--- CHZZK_Hide_Blind_Chat_VOD/CHZZK_Hide_Blind_Chat_VOD.user.js
+++ CHZZK_Hide_Blind_Chat_VOD/CHZZK_Hide_Blind_Chat_VOD.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         CHZZK Hide Blind Chat VOD
 // @namespace    CHZZK_Hide_Blind_Chat_VOD
-// @version      0.0.1
+// @version      0.0.2
 // @description  Hide messages that have been blinded in the VOD chat
 // @author       Nomo
 // @match        https://chzzk.naver.com/*
@@ -9,28 +9,11 @@
 // @homepage     https://github.com/nomomo/Chzzk_Scripts/
 // @downloadURL  https://github.com/nomomo/Chzzk_Scripts/raw/main/CHZZK_Hide_Blind_Chat_VOD/CHZZK_Hide_Blind_Chat_VOD.user.js
 // @updateURL    https://github.com/nomomo/Chzzk_Scripts/raw/main/CHZZK_Hide_Blind_Chat_VOD/CHZZK_Hide_Blind_Chat_VOD.user.js
-// @require      https://cdnjs.cloudflare.com/ajax/libs/arrive/2.4.1/arrive.min.js
-// @grant        none
+// @grant        GM_addStyle
 // ==/UserScript==
 
 (function() {
     'use strict';
-
-    // Set debug flag (1 for debug mode, 0 for normal operation)
-    const debugFlag = 0;
-
-    // Use arrive.js to monitor for new elements that contain 'live_chatting_message_is_hidden__' in their class
-    document.arrive('[class*="live_chatting_message_is_hidden__"]', function(newElement) {
-        // Find the parent element with class containing 'vod_chatting_item__'
-        let parent = newElement.closest('[class*="vod_chatting_item__"]');
-        if (parent) {
-            if (debugFlag === 1) {
-                // If debug flag is 1, apply a red border with !important
-                parent.style.setProperty('border', '2px solid red', 'important');
-            } else {
-                // Otherwise, hide the parent element
-                parent.style.display = 'none';
-            }
-        }
-    });
+    const debugFlag = 0; // 1 for debug mode, 0 for normal operation
+    GM_addStyle(`[class*="vod_chatting_item__"]:has([class*="live_chatting_message_is_hidden__"]) { ${debugFlag === 1 ? "border: 2px solid red !important;" : "display: none;" } }`);
 })();
